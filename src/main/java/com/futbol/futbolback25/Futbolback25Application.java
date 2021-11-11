b5460d1c1a25a7cb241cfc05eacad9737ae78eeb
Combatt17 <carloscombatt17@gmail.com>
2021-11-10 19:48:39 -0500

    inicialización del proyecto

---
+++ src/main/java/com/futbol/futbolback25/Futbolback25Application.java
@@ -0,0 +1,13 @@
+package com.futbol.futbolback25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Futbolback25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Futbolback25Application.class, args);
+	}
+
+}
